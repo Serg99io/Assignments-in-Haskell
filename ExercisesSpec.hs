bc907075f22d7526c851d385747c39153a4543f8
Sergio Kazatzidis <s.kazatzidis@gmail.com>
2020-05-29 15:13:37 +0000

    some tests

--- ExercisesSpec.hs
+++ ExercisesSpec.hs
@@ -8,4 +8,11 @@ spec = do
     describe "sum'" $ do
        it "twenty" $ do
          (sum' [7,8,5]) `shouldBe` 20
-       	  
+
+    describe "alltrue" $ do
+       it "all false" $ do
+         (alltrue [False,False,False]) `shouldBe` False
+       it "one true" $ do
+         (alltrue [True,False,False]) `shouldBe` False
+       it "all true" $ do
+         (alltrue [True,True,True]) `shouldBe` True
